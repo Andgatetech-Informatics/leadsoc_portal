d2e77875df2f24af2badf436bcd72cccbdf744c4
Madhavi Singh <01singh.madhavi@gmail.com>
2026-01-16 15:31:55 +0530

    minor fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,11 @@
-export const baseUrl = "http://192.168.1.102.:4000";
+// export const baseUrl = "http://192.168.1.102.:4000";
 // export const baseUrl = "http://localhost:4000";
 // export const baseUrl = "http://122.160.167.200:4000";
 export const frontendUrl = "https://myportal.andgatetech.com";
 export const candidateRegistrationUrl =
   "https://myportal.andgatetech.com/candidate-registration";
 
-// export const baseUrl = "https://myportal.andgatetech.com/backend";
+export const baseUrl = "https://myportal.andgatetech.com/backend";
 
 export const companyLogoText = "LeadSoc";
 export const companyName = "LeadSoc Technologies Pvt Ltd";
